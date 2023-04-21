3044644dad5dfed84972ba3d13b3296da72b31a3
Patrick W <321buildablock123@gmail.com>
2023-04-21 15:01:28 -0700

    working example of writing file to supabase

--- src/api/conversations.py
+++ src/api/conversations.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from src import database as db
 from pydantic import BaseModel
 from typing import List
@@ -19,6 +19,9 @@ class ConversationJson(BaseModel):
 
 
 router = APIRouter()
+
+# def addConversationToCSV():
+
 
 
 @router.post("/movies/{movie_id}/conversations/", tags=["movies"])
@@ -42,18 +45,40 @@ def add_conversation(movie_id: int, conversation: ConversationJson):
     # how you could implement persistent storage.
 
     # print(conversation)
-    response = {"my placeholder response"}
+    response = {"my null response ._."}
+
+    #Step 1: ensure that both characters are part of the movie
+    if db.characters[conversation.character_1_id][1] == movie_id and db.characters[conversation.character_2_id][1] == movie_id and conversation.character_1_id != conversation.character_2_id:
+        #both characters are in the movie
+        print("both characters are in the movie")
+        response = {"made it to step 1 :D"}
+
+        print(conversation)
+
+        #Step 2: go through the lines and check that the speakers match the characters provided
+        # for line in conversation.lines:
+        #     if conversation.lines[line].character_id != conversation.character_1_id and conversation.lines[line].character_id != conversation.character_2_id:
+        #         raise HTTPException(status_code=404, detail="step 2 failure: a line is spoken by a character not in the conversation")
+        
+        for index, line in enumerate(conversation.lines):
+            if line.character_id != conversation.character_1_id and line.character_id != conversation.character_2_id:
+                raise HTTPException(status_code=400, detail="step 2 failure:a line is spoken by a character not in the conversation")
+        
+        #Step 3: add the conversation to the database
+        response = {"made it to step 3 :D"}
+
+        #break out into lines and conversations
+        #determine highest convo ID
+        
+        #Test writing to movie_conversations_log.csv
+        print(db.logs)
+        db.logs.append({'post_call_time': '2023-04-21 14:44:31', 'movie_id_added_to': 525})
+        print(db.logs)
+
+        db.upload_new_log()
+
+
+    else:
+        raise HTTPException(status_code=404, detail="One or more characters not found in the referenced movie")
 
     return response
-
-    # #Step 1: ensure that both characters are part of the movie
-    # if db.characters[conversation["character_1_id"]][1] == movie_id and db.characters[conversation["character_2_id"]][1] == movie_id and conversation["character_1_id"] != conversation["character_2_id"]:
-    #     #both characters are in the movie
-    #     print("both characters are in the movie")
-    #     response = {"my placeholder response"}
-
-    #     #Step 2: go through the lines and check that the speakers match the characters provided
-    # else:
-    #     raise HTTPException(status_code=404, detail="One or more characters not found in the referenced movie")
-
-    # return response
